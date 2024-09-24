e60c9367396e44656b4be56b9f5974e6dd580ca4
Qamar Aziz <azizqamar7@gmail.com>
2024-09-24 21:05:08 +0530

    Increased Marquee Ix

--- src/helpers/marquee-scroll.js
+++ src/helpers/marquee-scroll.js
@@ -3,5 +3,5 @@ import gsap from 'gsap'
 export const marqueeScrollIx = () => {
   const tl = gsap.timeline({ repeat: -1 })
 
-  tl.to('[marquee-track]', { x: '-50%', ease: 'none', duration: 30 })
+  tl.to('[marquee-track]', { x: '-50%', ease: 'none', duration: 50 })
 }
