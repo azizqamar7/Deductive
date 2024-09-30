59fea655593e240cf2df4de2502405c45937d444
Qamar Aziz <azizqamar7@gmail.com>
2024-09-30 20:23:09 +0530

    Added BG change Ix

--- src/home/section-secure.js
+++ src/home/section-secure.js
@@ -1,21 +1,21 @@
 import gsap from 'gsap'
 
 export const sectionSecure = () => {
-  const darkTl = gsap.timeline()
+  //   const darkTl = gsap.timeline()
 
-  darkTl
-    .to('.section_home_secure', {
-      backgroundColor: '#061611',
-      color: '#a8b5ad',
-      duration: 0.4,
-      ease: 'power2.out',
-    })
-    .to('.section_home_secure h2', { color: '#fff' }, 0)
-    .to(
-      '.section_home_secure .text-color-dark-green-light',
-      { color: '#348e77' },
-      0
-    )
+  //   darkTl
+  // .to('.section_home_secure', {
+  //   backgroundColor: '#061611',
+  //   color: '#a8b5ad',
+  //   duration: 0.4,
+  //   ease: 'power2.out',
+  // })
+  // .to('.section_home_secure h2', { color: '#fff' }, 0)
+  // .to(
+  //   '.section_home_secure .text-color-dark-green-light',
+  //   { color: '#348e77' },
+  //   0
+  // )
 
   //   const lightTl = gsap.timeline()
 
@@ -37,12 +37,25 @@ export const sectionSecure = () => {
     scrollTrigger: {
       trigger: '.section_home_secure',
       start: 'top 0%',
-      onEnter: () => {
-        darkTl.restart()
-      },
+      //   onEnter: () => {
+      //     darkTl.play()
+      //   },
       //   onLeaveBack: () => {
       //     lightTl.restart()
       //   },
     },
   })
+
+  tl.to('.section_home_secure', {
+    backgroundColor: '#061611',
+    color: '#a8b5ad',
+    duration: 0.4,
+    ease: 'power2.out',
+  })
+    .to('.section_home_secure h2', { color: '#fff' }, 0)
+    .to(
+      '.section_home_secure .text-color-dark-green-light',
+      { color: '#348e77' },
+      0
+    )
 }
